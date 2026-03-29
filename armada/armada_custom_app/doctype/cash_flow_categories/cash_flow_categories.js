e9be35267166d1d07e4bfa6587fd067e83f8e0ef
Munisa33 <munisabax2002@gmail.com>
2026-03-29 13:59:54 +0500

    full accounts in cash category

--- armada/armada_custom_app/doctype/cash_flow_categories/cash_flow_categories.js
+++ armada/armada_custom_app/doctype/cash_flow_categories/cash_flow_categories.js
@@ -3,10 +3,6 @@
 
 frappe.ui.form.on("Cash Flow Categories", {
 	refresh(frm) {
-		frm.set_query("direct_expence_account", "direct_cash_flow", function() {
-			return {
-				query: "armada.armada_custom_app.doctype.cash_flow_categories_item.cash_flow_categories_item.get_indirect_accounts"
-			};
-		});
+
 	},
 });
