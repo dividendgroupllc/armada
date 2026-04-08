c991f4a4f9b6aa1677bab0657283df50854af379
Sherzod Rohatov <YOUR_GITHUB_EMAIL>
2026-04-08 22:42:07 +0000

    Fix UI bugs

--- armada/public/dist/js/armada-dashboard.bundle.JPN2VWBZ.js
+++
@@ -1,5 +0,0 @@
-(() => {
-  // ../armada/armada/public/js/armada-dashboard.bundle.js
-  console.log("Armada Dashboard JS loaded");
-})();
-//# sourceMappingURL=armada-dashboard.bundle.JPN2VWBZ.js.map
